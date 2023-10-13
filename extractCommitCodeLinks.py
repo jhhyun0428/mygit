2a7061af718f82b496a1dccd5c918abadada51f6
saleese-gnu <80230270+saleese-gnu@users.noreply.github.com>
2023-10-13 10:48:20 +0900

    Update extractCommitCodeLinks.py
    added token files and others.

--- extractCommitCodeLinks.py
+++ extractCommitCodeLinks.py
@@ -1,20 +1,21 @@
 from github import Github
 
 # Authenticate with GitHub API using personal access token
-g = Github("token")
+f = open("token.txt", 'r')
+token = f.readline()
+f.close()
+
+g = Github(token)
 
 # Get the VS Code repository object
-repo = g.get_repo("elastic-search")
+repo = g.get_repo("elastic/elasticsearch")
 
 # Get all the commits in the repository
 commits = repo.get_commits()
 
 # Iterate through the commits and print their SHA and commit message
 for commit in commits:
-    print(f"{commit.sha} - {commit.commit.message}")
-    tree = commit.tree
-    # Replace <file_extension> with the actual extension you want to extract, e.g. ".py"
-    source_files = [f for f in tree if f.type == "blob"]
-    for f in source_files:
-        print(f)
-
+    files = commit.files
+    for file in files:
+        print(f"{commit.sha}, {file.filename}")    
+    break
