7fa50f36daf5f9e84b409dd372ff956feedabe37
saleese-gnu <saleese@gnu.ac.kr>
2023-12-31 23:13:14 +0900

    added two files

---
+++ test/src/main/java/kr/ac/gnu/selab/test/App01.java
@@ -0,0 +1,71 @@
+package kr.ac.gnu.selab.test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.resolution.UnsolvedSymbolException;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+
+public class App01 {
+
+	public static void main(String[] args) throws IOException {
+		final Path SOURCE_ROOT_PATH = Path.of("C:\\Users\\user\\git\\elasticsearch");
+		final Path TARGET_PATH = Path.of("C:\\Users\\user\\git\\elasticsearch\\test\\framework\\src\\test\\java\\org\\elasticsearch\\cluster\\service\\FakeThreadPoolMasterServiceTests.java");
+
+		JavaSymbolSolver javaSymbolSolver = new JavaSymbolSolver(new JavaParserTypeSolver(SOURCE_ROOT_PATH));
+		StaticJavaParser.getParserConfiguration().setSymbolResolver(javaSymbolSolver);
+
+
+		CompilationUnit cu 
+		= StaticJavaParser.parse(TARGET_PATH);
+		Optional<ClassOrInterfaceDeclaration> classA
+		= cu.getClassByName("A");
+
+		//	      cu.findAll(FieldDeclaration.class)
+		//	      .stream()
+		//	      .filter(f -> f.isPublic() && !f.isStatic())
+		//	      .forEach(f -> System.out.println("Check field at line " +
+		//	            f.getRange().map(r -> r.begin.line).orElse(-1)));
+
+		MethodVisitor visitor = new MethodVisitor();
+
+
+		List<MethodDeclaration> methodDeclarationList = new ArrayList<>();
+		visitor.visit(cu, methodDeclarationList);
+
+		methodDeclarationList.forEach(md -> {
+			List<MethodCallExpr> methodCallExprList = md.findAll(MethodCallExpr.class);
+			methodCallExprList.forEach(methodCallExpr -> {
+				try {
+					if (md.resolve() == null) {
+						return;
+					}
+					if (methodCallExpr.resolve() == null) {
+						return;
+					}
+
+				}
+				catch (UnsolvedSymbolException e) {
+					;
+				}
+				try {
+					System.out.println(md.resolve().getQualifiedSignature() + ", " 
+							+ methodCallExpr /*.resolve().getQualifiedSignature() */);
+				}
+				catch (UnsolvedSymbolException e) {
+					;
+				}
+			});
+		});
+	}
+
+}
---
+++ test/src/main/java/kr/ac/gnu/selab/test/App02.java
@@ -0,0 +1,60 @@
+package kr.ac.gnu.selab.test;
+
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App02
+{
+	public static void main(String[] args ) throws IOException 
+	{
+		final Path SOURCE_ROOT_PATH = Path.of(System.getProperty("user.dir"), "src", "main", "java");
+		final Path TARGET_PATH = Path.of(System.getProperty("user.dir"),"src","main", "java", "Target.java");
+		System.out.println(TARGET_PATH);
+
+		JavaSymbolSolver javaSymbolSolver = new JavaSymbolSolver(new JavaParserTypeSolver(SOURCE_ROOT_PATH));
+		StaticJavaParser.getParserConfiguration().setSymbolResolver(javaSymbolSolver);
+
+
+		CompilationUnit cu 
+		= StaticJavaParser.parse(TARGET_PATH);
+		Optional<ClassOrInterfaceDeclaration> classA
+		= cu.getClassByName("A");
+
+//		cu.findAll(FieldDeclaration.class)
+//		.stream()
+//		.filter(f -> f.isPublic() && !f.isStatic())
+//		.forEach(f -> System.out.println("Check field at line " +
+//				f.getRange().map(r -> r.begin.line).orElse(-1)));
+
+		MethodVisitor visitor = new MethodVisitor();
+
+
+		List<MethodDeclaration> methodDeclarationList = new ArrayList<>();
+		visitor.visit(cu, methodDeclarationList);
+
+		methodDeclarationList.forEach(md -> {
+			List<MethodCallExpr> methodCallExprList = md.findAll(MethodCallExpr.class);
+			methodCallExprList.forEach(methodCallExpr -> {
+				System.out.println(md.resolve().getQualifiedSignature() + ", " 
+						+ methodCallExpr.resolve().getQualifiedSignature());
+			});
+		});
+	}
+}
