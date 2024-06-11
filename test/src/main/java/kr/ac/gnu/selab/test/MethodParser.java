6b76e4ef3082802ea848eedc3b25a97771543786
saleese-gnu <saleese@gnu.ac.kr>
2024-06-11 10:03:24 +0900

    separated ClassCallVisitor from MethodParser

--- test/src/main/java/kr/ac/gnu/selab/test/MethodParser.java
+++ test/src/main/java/kr/ac/gnu/selab/test/MethodParser.java
@@ -23,27 +23,24 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSol
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
-//
 
 public class MethodParser {
-
-	static String Caller = ""; // caller name will be set later
 
 	static String path = "/Users/seonahlee/git/elasticsearch";
 	//	static String Path = "C:\\Users\\hyun\\Desktop\\elasticsearch-main";	
 
 	static boolean Os = true; // false: windows, true: Mac
 
-	static List<String> file_find_paths = findJavaFiles(path);
-
 	//static List<String> file_find_paths = findJavaFiles("C:\\Users\\hyun\\Desktop\\elasticsearch-main");
 
 	public static void main(String[] args) {
+		
+		MethodParser methodParser = new MethodParser();
 
 		String startDirectory = path;  // 시작할 디렉토리 경로 입력
 		//    	String startDirectory = "C:\\Users\\hyun\\Desktop\\elasticsearch-main";  // 시작할 디렉토리 경로 입력
 		Set<String> javaPaths = new HashSet<>();
-		findJavaFiles(new File(startDirectory), javaPaths);
+		methodParser.findJavaFiles(new File(startDirectory), javaPaths);
 
 		CombinedTypeSolver typeSolver = new CombinedTypeSolver();
 		typeSolver.add(new ReflectionTypeSolver());
@@ -66,19 +63,19 @@ public class MethodParser {
 
 		for (String path_d : directories_test) {
 
-			List<String> file_paths = findJavaFiles(path_d);
+			List<String> file_paths =methodParser.findJavaFiles(path_d);
 
 
 			for (String fp : file_paths) {
 				//       		System.out.println(fp);
-				finder(fp);
+				methodParser.finder(fp);
 			}
 
 		}
 
 	}
 
-	private static void findJavaFiles(File directory, Set<String> javaPaths) {
+	private void findJavaFiles(File directory, Set<String> javaPaths) {
 		File[] files = directory.listFiles();
 
 		if (files == null) return;  // 파일이나 디렉토리가 없는 경우
@@ -87,7 +84,7 @@ public class MethodParser {
 			if (file.isDirectory()) {
 				findJavaFiles(file, javaPaths);
 			} else if (file.getName().endsWith(".java")) {
-				String pathToJavaFolder = findNearestJavaFolder(file.getParentFile());
+				String pathToJavaFolder = this.findNearestJavaFolder(file.getParentFile());
 				if (pathToJavaFolder != null) {
 					javaPaths.add(pathToJavaFolder);
 				}
@@ -95,7 +92,7 @@ public class MethodParser {
 		}
 	}
 
-	private static String findNearestJavaFolder(File directory) {
+	private String findNearestJavaFolder(File directory) {
 		while (directory != null) {
 			if (directory.getName().equalsIgnoreCase("java")) {
 				return directory.getPath();
@@ -106,7 +103,7 @@ public class MethodParser {
 	}
 
 	// 메소드가 선언된 클래스의 이름을 찾는 메소드
-	private static String findClassName(MethodDeclaration method) {
+	private String findClassName(MethodDeclaration method) {
 		Node currentNode = method.getParentNode().orElse(null);
 		while (currentNode != null) {
 			if (currentNode instanceof ClassOrInterfaceDeclaration) {
@@ -116,52 +113,15 @@ public class MethodParser {
 		}
 		return "";
 	}
-
-	private static class ClassCallVisitor extends VoidVisitorAdapter<Void> {
-
-		@Override
-		public void visit(MethodDeclaration n, Void arg) {
-			//            System.out.println("Method Name: " + n.resolve().getQualifiedSignature());
-			Caller = n.resolve().getQualifiedSignature();
-			super.visit(n, arg);
-		}
-
-		@Override
-		public void visit(ObjectCreationExpr n, Void arg) {
-			//System.out.println("Class Instance Created: " + n.getType().getName());
-			super.visit(n, arg);
-		}
-
-		@Override
-		public void visit(MethodCallExpr n, Void arg) {
-			//            n.getScope().ifPresent(scope -> System.out.println("Method Call: " + scope + "." + n.getName()));
-
-			try {
-				String qualifiedName = n.resolve().getQualifiedSignature();
-				//                if (!qualifiedName.contains("java.lang"))
-				System.out.println(Caller + ", " + qualifiedName);
-				String Caller_f = buildJavaFileName(Caller);
-				System.out.println(Caller_f + " , " + findFilePath(file_find_paths, Caller_f));
-				String Callee_f = buildJavaFileName(qualifiedName);
-				System.out.println(Callee_f+ " , " +findFilePath(file_find_paths, Callee_f));
-
-			} catch (Exception e) {
-				//                System.out.println("==> Could not resolve method call: " + n);
-			}
-
-			super.visit(n, arg);
-		}
-	}
-
-	public static void finder(String path_s) {
-
+	
+	public void finder(String path_s) {
 
 		// Now parse a file and resolve symbols
 		try {
 			CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(path_s));
 
 			// Use visitors or other methods to analyze the code and resolve symbols
-			cu.accept(new ClassCallVisitor(), null);
+			cu.accept(new ClassCallVisitor(this, path), null);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch(UnsolvedSymbolException e) {
@@ -176,13 +136,13 @@ public class MethodParser {
 		}
 	}
 
-	public static List<String> findJavaFiles(String folderPath) {
+	public List<String> findJavaFiles(String folderPath) {
 		List<String> javaFiles = new ArrayList<>();
 		findJavaFilesInFolder(new File(folderPath), javaFiles);
 		return javaFiles;
 	}
 
-	private static void findJavaFilesInFolder(File folder, List<String> javaFiles) {
+	private void findJavaFilesInFolder(File folder, List<String> javaFiles) {
 		// 폴더 내의 모든 파일과 하위 폴더 확인
 		File[] files = folder.listFiles();
 		if (files != null) {
@@ -200,7 +160,7 @@ public class MethodParser {
 		}
 	}
 
-	public static String findFilePath(List<String> filePaths, String fileName) {
+	public String findFilePath(List<String> filePaths, String fileName) {
 		for (String filePath : filePaths) {
 			if (filePath.endsWith(fileName)) {
 				return filePath;
@@ -209,7 +169,7 @@ public class MethodParser {
 		return null; // 파일을 찾지 못한 경우
 	}
 
-	public static String buildJavaFileName(String originalString) {
+	public String buildJavaFileName(String originalString) {
 		// 문자열을 '.'을 기준으로 분할
 		String regex = "\\([^)]*\\)";
 		String stringWithoutParentheses = originalString.replaceAll(regex, "");
@@ -226,4 +186,6 @@ public class MethodParser {
 			throw new IllegalArgumentException("Invalid input string");
 		}
 	}
+
+
 }
