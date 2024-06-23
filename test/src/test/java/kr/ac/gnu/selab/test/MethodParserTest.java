c86d9ab4e7e40a3e321d06cd187b4c7e6c820d39
jhhyun0428 <69348443+jhhyun0428@users.noreply.github.com>
2024-06-24 01:56:04 +0900

    Create MethodParserTest.java

---
+++ test/src/test/java/kr/ac/gnu/selab/test/MethodParserTest.java
@@ -0,0 +1,25 @@
+package kr.ac.gnu.selab.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class MethodParserTest {
+
+	@Test
+	void testFindJavaFiles() {
+		
+		String path = "C:\\Users\\user\\Desktop\\elasticsearch-main";
+		String pathAfter = "\\plugins\\mapper-size\\src\\internalClusterTest\\java\\org\\elasticsearch\\index\\mapper\\size";
+		
+		MethodParser methodParser = new MethodParser();
+		List<String> testJavaFolder = new ArrayList<>();
+		testJavaFolder = methodParser.findJavaFiles(path+pathAfter);
+		
+		assertEquals(1, testJavaFolder.size());//폴더 안에 파일 하나.
+	}
+
+}
